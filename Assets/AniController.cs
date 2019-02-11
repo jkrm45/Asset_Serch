797abdd46d24982307b5ed8ead1235f6de2832fd
jkrm45 <44226739+jkrm45@users.noreply.github.com>
2019-02-11 15:18:15 +0900

    jkrm45
    dsdsdsd

---
+++ Assets/AniController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniController : MonoBehaviour
+{
+    private static AniController _instance = null;
+    public static AniController Instance { get { return _instance; } }
+    public Animator AniControll;
+    public int Job = 1;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    void Start()
+    {
+
+        AniControll = GameObject.Find("Player").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Chrickidle();
+    }
+
+   public void Chrickidle() //애니메이션 종류선택
+    {
+        if (Job == 1)
+        {
+            AniControll.SetBool("SwardMan", true);
+            AniControll.SetBool("Archer", false);
+            AniControll.SetBool("Magic", false);
+
+        }
+        if (Job == 2)
+        {
+            AniControll.SetBool("SwardMan", false);
+            AniControll.SetBool("Archer", true);
+            AniControll.SetBool("Magic", false);
+        }
+        if (Job == 3)
+        {
+            AniControll.SetBool("SwardMan", false);
+            AniControll.SetBool("Archer", false);
+            AniControll.SetBool("Magic", true);
+        }
+    }
+}
